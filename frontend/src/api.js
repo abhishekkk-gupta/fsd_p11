9c95e25ad455fb9af8a1feeb00cf90a8fc7996a4
Abhishek Gutpa <guptaabhi0520@gmail.com>
2026-03-22 23:07:07 +0530

    updated API URL to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://fsd-p11-grocery-backend.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
